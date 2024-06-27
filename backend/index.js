1e09a9512716bb1924b941e73906bb8bb5b08a65
kkalyankumar9 <kummarikalyan010@gmail.com>
2024-06-27 23:18:50 +0530

    backend issue resolved

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const cors=require("cors")
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = 8080;
 
 app.use(express.json());
 app.use(cors())
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import TaskManger from './Components/taskManger';
 function App() {
   return (
     <div>
-     <p className='text-blue-400 text-9xl'>data</p>
+     
      <TaskManger/>
     </div>
   );
