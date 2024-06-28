098fed27d3bea838c0b1e32a35fdc02f65d26217
kkalyankumar9 <kummarikalyan010@gmail.com>
2024-06-28 07:14:02 +0530

    backend port error solving

--- backend/db.js
+++ backend/db.js
@@ -2,6 +2,7 @@ const { Sequelize } = require("sequelize");
 
 const database = new Sequelize("task_manager", "root", "Kalyan@99", {
     host: "localhost",
+    port: 3306, 
     dialect: "mysql",
   });
   
